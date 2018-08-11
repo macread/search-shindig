8cc9ea085e36f213f687f255628a8fc2552e10b1
James 'Mac' Read <james.macgregor.read@gmail.com>
2018-08-10 22:08:10 -0600

    Loading 25 picture, added nodemon.json

--- server/controller.js
+++ server/controller.js
@@ -2,21 +2,19 @@
 const axios = require('axios');
 require('dotenv').config();
 
-// needed for S3 processing
 const {
-	USER_NAME,
-	API_ACCESS_KEY
+	API_KEY
 } = process.env;
 
 module.exports = {
     // User Callbacks
 
-    getPictures: (req, res) => {
-        axios(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_ACCESS_KEY}dd&text="${req.data}"&per_page=25`)
+    searchPictures: (req, res) => {
+        axios(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&text="${req.query.text}"&per_page=25&format=json&nojsoncallback=1`)
             .then ( (results) => {
-                // console.log(results.data)
-                res.status(200).send(results.data)} )
+                res.status(200).send(results.data) })
             .catch ( (err) => res.status(500).send())
     },
+
 
 }
--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
 
 const styles = theme => ({
   container: {
@@ -19,25 +26,69 @@ const styles = theme => ({
 });
 
 class App extends Component {
+
+    constructor() {
+    super()
+        
+        this.state = {
+			searchCriteria: '',
+			photos: []
+        }
+    }
+
+    updateSearchCriteria(val){
+        this.setState({ searchCriteria: val})
+	}
+	
+	searchPictures(){
+		axios(`/api/search/?text=${this.state.searchCriteria}`)
+			.then( result => this.setState({photos: result.data.photos.photo}) )
+	}
+
   render() {
     const { classes } = this.props;
 
     return (
 
-      <div className="App">
+        <div className="App">
+            <form className={classes.container} noValidate autoComplete="off">
 
+                <TextField
+                id="search"
+                label="Search field"
+                type="search"
+                className={classes.textField}
+                margin="normal"
+                value={this.state.searchField}
+                onChange={ (e) => this.updateSearchCriteria(e.target.value)}
+                />
 
-      <form className={classes.container} noValidate autoComplete="off">
+				<Button variant="contained" color="primary" className={classes.button} onClick={() => {this.searchPictures()}}>
+					Search
+				</Button>
 
-        <TextField
-          id="search"
-          label="Search field"
-          type="search"
-          className={classes.textField}
-          margin="normal"
-        />
-      </form>
-      </div>
+				<GridList cellHeight={180} className={classes.gridList}>
+					{/* <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}> */}
+					<GridListTile key="Subheader" style={{ height: '250px' }}> 
+					<ListSubheader component="div">Photos from Flickr API. All photographs copyright by thier respective oweners.</ListSubheader>
+					</GridListTile>
+					{this.state.photos.map(photo => (
+					<GridListTile key={photo.id}>
+						<img src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} alt={photo.title} />
+						<GridListTileBar
+						title={photo.title}
+						// subtitle={<span>by: {tile.author}</span>}
+						// actionIcon={
+						// 	<IconButton className={classes.icon}>
+						// 	<InfoIcon />
+						// 	</IconButton>
+						// }
+						/>
+					</GridListTile>
+					))}
+      			</GridList>
+            </form>
+        </div>
     );
   }
 }
